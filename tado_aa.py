a8c066664581247b6b906fe138ff62ed19392171
adrianslabu <adrianslabu@icloud.com>
2021-02-14 22:50:41 +0100

    Update tado_aa.py
    - Fixed the "stack depth limit" error
    - Added date and time before comments

--- tado_aa.py
+++ tado_aa.py
@@ -5,7 +5,9 @@
 
 import sys
 import time
+import inspect
 
+from datetime import datetime
 from PyTado.interface import Tado
 
 def main():
@@ -21,8 +23,8 @@ def main():
     username = "your_username@mail.com" # tado username
     password = "your_password" # tado password
 
-    checkingInterval = 6.0 # checking interval (in seconds)
-    errorRetringInterval = 20.0 # retrying interval (in seconds), in case of an error
+    checkingInterval = 10.0 # checking interval (in seconds)
+    errorRetringInterval = 30.0 # retrying interval (in seconds), in case of an error
 
     login()
     homeStatus()
@@ -51,7 +53,7 @@ def login():
             login()    
 
 def homeStatus():
-
+    
     global devicesHome
 
     try:
@@ -102,7 +104,7 @@ def homeStatus():
         devicesHome.clear()
         printm ("Waiting for a change in devices location or for an open window..")
         time.sleep(1)
-        geofencing()
+        engine()
 
     except KeyboardInterrupt:
         printm ("Interrupted by user.")
@@ -112,94 +114,81 @@ def homeStatus():
         if (str(e).find("location") != -1):
             printm ("I cannot get the location of one of the devices because the user signed out from tado app.\nWaiting for the user to sign in, until then the Geofencing Assist is NOT active.\nWaiting for an open window..")
             time.sleep(1)
-            openWindowDetection()
+            engine()
         elif (str(e).find("NoneType") != -1):
             time.sleep(1)
-            openWindowDetection()
+            engine()
         else:
             printm (str(e) + "\nConnection Error, retrying in " + str(errorRetringInterval) + " sec..")
             time.sleep(errorRetringInterval)
             homeStatus()
 
-def openWindowDetection():
+def engine():
 
-    try:
-        for z in t.getZones():
-            zoneID = z["id"]
-            zoneName = z["name"]
-            if (t.getOpenWindowDetected(zoneID)["openWindowDetected"] == True):
-                printm (zoneName + ": open window detected, activating the OpenWindow mode.")
-                t.setOpenWindow(zoneID)
+    i = 0
+    while i < 3: # My solution to fix the "stack depth"
+        try:
+            i = 0
+            #Open Window Detection
+            for z in t.getZones():
+                    zoneID = z["id"]
+                    zoneName = z["name"]
+                    if (t.getOpenWindowDetected(zoneID)["openWindowDetected"] == True):
+                        printm (zoneName + ": open window detected, activating the OpenWindow mode.")
+                        t.setOpenWindow(zoneID)
+                        printm ("Done!")
+                        printm ("Waiting for a change in devices location or for an open window..")
+            #Geofencing
+            homeState = t.getHomeState()["presence"]
+
+            for mobileDevice in t.getMobileDevices():
+                if (mobileDevice["location"]["relativeDistanceFromHomeFence"] == 0.0):
+                    devicesHome.append(mobileDevice["name"]) 
+
+            if (lastMessage.find("Connection Error") != -1 or lastMessage.find("Waiting for the user to sign in") != -1):
+                printm ("Connection established, everything looks good now, continuing..\n")
+                printm ("Waiting for a change in devices location or for an open window..")
+
+            if (len(devicesHome) > 0 and homeState == "AWAY"):
+                if (len(devicesHome) == 1):
+                    printm (devicesHome[0] + " is at home, activating HOME mode.")
+                else:
+                    devices = ""
+                    for i in range(len(devicesHome)):
+                        if (i != len(devicesHome) - 1):
+                            devices += devicesHome[i] + ", "
+                        else:
+                            devices += devicesHome[i]
+                    printm (devices + " are at home, activating HOME mode.")
+                t.setHome()
                 printm ("Done!")
                 printm ("Waiting for a change in devices location or for an open window..")
 
-        geofencing()
+            elif (len(devicesHome) == 0 and homeState == "HOME"):
+                printm ("Are no devices at home, activating AWAY mode.")
+                t.setAway()
+                printm ("Done!")
+                printm ("Waiting for a change in devices location or for an open window..")
 
-    except KeyboardInterrupt:
-        printm ("Interrupted by user.")
-        sys.exit(0)
-
-    except Exception as e:
-        printm (str(e) + "\nConnection Error, retrying in " + str(errorRetringInterval) + " sec..")
-        time.sleep(errorRetringInterval)
-        openWindowDetection()
-
-def geofencing():
-
-    try:
-        homeState = t.getHomeState()["presence"]
-
-        for mobileDevice in t.getMobileDevices():
-            if (mobileDevice["location"]["relativeDistanceFromHomeFence"] == 0.0):
-                devicesHome.append(mobileDevice["name"]) 
-
-        if (lastMessage.find("Connection Error") != -1 or lastMessage.find("Waiting for the user to sign in") != -1):
-            printm ("Connection established, everything looks good now, continuing..\n")
-            printm ("Waiting for a change in devices location or for an open window..")
-        
-
-        if (len(devicesHome) > 0 and homeState == "AWAY"):
-            if (len(devicesHome) == 1):
-                printm (devicesHome[0] + " is at home, activating HOME mode.")
-            else:
-                devices = ""
-                for i in range(len(devicesHome)):
-                    if (i != len(devicesHome) - 1):
-                        devices += devicesHome[i] + ", "
-                    else:
-                        devices += devicesHome[i]
-                printm (devices + " are at home, activating HOME mode.")
-            t.setHome()
-            printm ("Done!")
-            printm ("Waiting for a change in devices location or for an open window..")
-
-        elif (len(devicesHome) == 0 and homeState == "HOME"):
-            printm ("Are no devices at home, activating AWAY mode.")
-            t.setAway()
-            printm ("Done!")
-            printm ("Waiting for a change in devices location or for an open window..")
-
-        devicesHome.clear()
-        time.sleep(checkingInterval)
-        openWindowDetection()
-
-    except KeyboardInterrupt:
-        printm ("Interrupted by user.")
-        sys.exit(0)
-
-    except Exception as e:
-        if (str(e).find("location") != -1 or str(e).find("NoneType") != -1):
-            printm ("I cannot get the location of one of the devices because the user signed out from tado app.\nWaiting for the user to sign in, until then the Geofencing Assist is NOT active.\nWaiting for an open window..")
+            devicesHome.clear()
             time.sleep(checkingInterval)
-        else:
-            printm (str(e) + "\nConnection Error, retrying in " + str(errorRetringInterval) + " sec..")
-            time.sleep(errorRetringInterval)
-        openWindowDetection()
+
+        except KeyboardInterrupt:
+                printm ("Interrupted by user.")
+                sys.exit(0)
+
+        except Exception as e:
+                if (str(e).find("location") != -1 or str(e).find("NoneType") != -1):
+                    printm ("I cannot get the location of one of the devices because the user signed out from tado app.\nWaiting for the user to sign in, until then the Geofencing Assist is NOT active.\nWaiting for an open window..")
+                    time.sleep(checkingInterval)
+                else:
+                    printm (str(e) + "\nConnection Error, retrying in " + str(errorRetringInterval) + " sec..")
+                    time.sleep(errorRetringInterval)
 
 def printm(message):
     global lastMessage
     if (message != lastMessage):
         lastMessage = (message)
-        sys.stdout.write(message + "\n")
+        sys.stdout.write(datetime.now().strftime('%d-%m-%Y %H:%M:%S') + " # " + message + "\n")
 
 main()
